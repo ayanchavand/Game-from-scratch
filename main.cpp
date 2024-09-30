cb23f3e963d0f7f93f8d0d66f40a3db8351cd9e5
ayanchavand <ayan.chavand15@gmail.com>
2024-09-30 17:05:53 +0530

    [feat] sound fx

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 
 int main()
 {   
@@ -11,29 +12,36 @@ int main()
     sf::Font font;
     if (!font.loadFromFile("font.ttf")) {
         std::cout << "Could not load fonts!" << std::endl;
-
     }
+    sf::SoundBuffer fx;
+    fx.loadFromFile("fx.wav");
+    sf::Sound pop;
+    pop.setBuffer(fx);
+
     sf::Text score;
     score.setFont(font);
     score.setCharacterSize(10);
     score.setFillColor(sf::Color::White);
 
     sf::Vector2f ballVel(1, 1);
+    
     sf::CircleShape ball(10);
     ball.setPosition(sf::Vector2f(1920 / 2, 1080 / 2));
 
     sf::RectangleShape paddle1(sf::Vector2f(500, 30));
-    paddle1.setPosition(sf::Vector2f(1920 / 2, 100));
+    paddle1.setPosition(sf::Vector2f(1920 / 2, 10));
     
     sf::RectangleShape paddle2(sf::Vector2f(500, 30));
-    paddle2.setPosition(sf::Vector2f(1920 / 2, 1080 - 100));
-        
-    sf::RectangleShape rect(sf::Vector2f(100, 100));
-
+    paddle2.setPosition(sf::Vector2f(1920 / 2, 1040));
+       
    // run the program as long as the window is open
     while (window.isOpen())
     {   
-        
+        /*
+        float fastfact = clock.getElapsedTime().asSeconds() * 1f;
+        ballVel.x = ballVel.x * fastfact;
+        ballVel.y = ballVel.y * fastfact;
+        */
         score.setString(std::to_string(clock.getElapsedTime().asSeconds()));
         // check all the window's events that were triggered since the last iteration of the loop
         sf::Event event;
@@ -43,9 +51,7 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
         } 
-
         window.clear();
-
         //ALl the Draw calls here
         window.draw(ball);
         window.draw(score);
@@ -55,7 +61,6 @@ int main()
         if (ball.getPosition().y <= 0 || ball.getPosition().y >= 1080) {
             ballVel.y = -ballVel.y;
         }
-        
         if (ball.getPosition().x <= 0 || ball.getPosition().x >= 1920) {
             ballVel.x = -ballVel.x;
         }
@@ -75,11 +80,13 @@ int main()
 
         if (ball.getGlobalBounds().intersects(paddle1.getGlobalBounds()) ||
             ball.getGlobalBounds().intersects(paddle2.getGlobalBounds())) {
-            ballVel.y = -ballVel.y;
+            ballVel.y = -ballVel.y;     
+            pop.play();
         }
         window.display();
     }
     std::cout << clock.restart().asSeconds();
+   
     return 0;
 }
     
