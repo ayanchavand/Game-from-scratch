91e76b97bed3e9d09fdbc7182317f56600e9248f
ayanchavand <ayan.chavand15@gmail.com>
2024-09-30 12:19:29 +0530

    [feat] window instantiation

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+int main()
+{   
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "Game");
+
+    while (window.isOpen()) {
+        sf::Event event;
+
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+    }
+    return 0;
+}
+    
